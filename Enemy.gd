66860123ce75cae9115586d8ad093b0a9625d714
flamechips <flamechips.dev@gmail.com>
2023-10-22 12:47:30 -0500

    Added motion input actions; switched to Input.get_vector() for player movement

--- Enemy.gd
+++ Enemy.gd
@@ -9,13 +9,13 @@ func _ready():
 	var randAngle = randi() % 360
 	var radius = 150
 	position = Vector2(sin(randAngle), cos(randAngle)) * radius
-	
+
 func _physics_process(delta):
 	NavAgent.target_position = Player.position
 	if !NavAgent.is_navigation_finished():
 		var direction = position.direction_to(NavAgent.get_next_path_position())
 		velocity = direction * moveSpeed
-	else: 
+	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
 	if get_last_slide_collision():
