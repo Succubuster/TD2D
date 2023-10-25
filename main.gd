a3346a1b58fc50f3551c74fd631e6702ac3ae25b
flamechips <77804370+flamechips@users.noreply.github.com>
2023-10-25 15:04:15 -0500

    Update to Godot 4.2b2 (#17)
    - Updating Godot version
    - Moved Enemy placement randomization into Main script, prior to placement in tree
    - Baked navmesh for Main scene

--- main.gd
+++ main.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 @onready var Enemy = preload("res://enemy.tscn")
+
+
 func _ready():
 	create_tween().set_loops().tween_callback(
 		func ():
-			add_child(Enemy.instantiate())
+			var enemy = Enemy.instantiate()
+			var randAngle = randi() % 360
+			var radius = 150
+			enemy.position = Vector2(sin(randAngle), cos(randAngle)) * radius
+			add_child(enemy)
 	).set_delay(1)
 
 func _input(event):
