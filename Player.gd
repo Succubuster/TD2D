65e4b083b88127500edddce851406de60b1b954a
Christopher King <cking5496@gmail.com>
2023-10-24 20:10:53 -0400

    Added AttackRangeAnchor
    - Added timer to control attack bool
    - Added collider to check for targets
    - Currently frees nearest "enemy"
    - Could be added to enemies later?

--- Player.gd
+++ Player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
 @export var moveSpeed: float = 100.0
-
+	
 func _physics_process(delta):
-	var inputVec = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
+	getInput()
+	move_and_slide()
+	
+func getInput():
+	var inputVec = Input.get_vector(
+		'move_left', 
+		'move_right', 
+		'move_up', 
+		'move_down'
+	)
 	if inputVec:
 		velocity = inputVec * moveSpeed
 	else:
 		velocity = Vector2.ZERO
-
-	move_and_slide()
